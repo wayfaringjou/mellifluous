ad53853eee60e5c33db5e9e75c41dd1e3530e0d7
Joel Del Cueto <wayfaringjou@gmail.com>
2020-11-05 00:19:17 -0500

    Add search form and listener, query request to API and results renderer

--- app.js
+++ app.js
@@ -26,14 +26,64 @@ function requestAccessToken() {
       }
       throw new Error(`Server response: ${response.status}`);
     })
-    .then((bodyJson) => bodyJson.access_token);
+    .then((bodyJson) => bodyJson.access_token)
+    .catch((error) => console.log(error.message));
+}
+
+function requestSongSearch(songQuery, token) {
+  const queryUrl = `https://api.spotify.com/v1/search?q=${encodeURIComponent(songQuery)}&type=track`;
+  const queryHeaders = new Headers({
+    Authorization: `Bearer ${token}`,
+  });
+  const queryOptions = {
+    method: 'GET',
+    headers: queryHeaders,
+  };
+
+  return fetch(queryUrl, queryOptions)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(`Server response: ${response.status}`);
+    })
+    .catch((error) => console.log(error.message));
+}
+
+function displaySongResults(tracksJson) {
+  const foundSongs = tracksJson.tracks.items;
+
+  $('#search-results-list').empty();
+
+  for (let i = 0; i < foundSongs.length; i++) {
+    $('#search-results-list').append(`
+    <li>
+    <h3>${foundSongs[i].name}</h3>
+    <h4>Artist(s)</h4>
+    <p>${foundSongs[i].artists.map((e) => e.name).join(', ')}<p>
+    <h4>Album</h4>
+    <p>${foundSongs[i].album.name}<p>
+    </li>`);
+  }
+}
+
+function handleSongSearchSubmit() {
+  $('#song-search').on('submit', (e) => {
+    e.preventDefault();
+    const songQuery = $(e.currentTarget).find('#song-search-input').val();
+    console.log(songQuery);
+
+    requestAccessToken()
+      .then((token) => requestSongSearch(songQuery, token))
+      .then((tracksJson) => displaySongResults(tracksJson));
+  });
 }
 
 // Load listeners when document is ready
 function handleAppLoad() {
-  // TODO: This is for testing, add proper listeners here
-  requestAccessToken()
-    .then((token) => console.log(token));
+  // Listen to song search form submission
+  handleSongSearchSubmit();
+  // TODO: Listen to clicking any of the songs on results list
 }
 // jQuery document ready load
 $(handleAppLoad());
