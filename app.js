7bc47cad0a4af2f9166dff8d6a4e0f717ef6a3d1
Joel Del Cueto <wayfaringjou@gmail.com>
2020-11-04 22:45:54 -0500

    Add jQuery document ready function call

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// TODO: Authorization function
+
+// Load listeners when document is ready
+function handleAppLoad() {
+  // TODO: Add listener functions
+}
+// jQuery document ready load
+$(handleAppLoad());
